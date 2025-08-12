21eb82411d1fab18ea7cbbd62762ef73ba4f8cba
Hankertrix <91734413+hankertrix@users.noreply.github.com>
2025-08-12 23:56:30 +0800

    Re-enable the CSP
    I give up on trying to fix the bug where Safari doesn't open the links
    in the results of the search bar.

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,54 +17,54 @@ export default defineConfig({
 	},
 
 	// CSP
-	// experimental: {
-	// 	csp: {
-	// 		algorithm: "SHA-512",
-	// 		directives: [
-	// 			"default-src 'none'",
-	// 			"img-src 'self'",
-	// 			"font-src 'self'",
-	// 			"connect-src 'self'",
-	// 			"manifest-src 'self'",
-	// 			"object-src 'none'",
-	// 			"frame-ancestors 'none'",
-	// 			"form-action 'none'",
-	// 			"base-uri 'none'",
-	// 		],
-	// 		scriptDirective: {
-	// 			resources: [
-	// 				"'self'",
-	//
-	// 				// For Pagefind to work
-	// 				"'wasm-unsafe-eval'",
-	// 			],
-	//
-	// 			hashes: [
-	// 				//
-	//
-	// 				// The script to use Pagefind to search the site
-	// 				"sha256-LzkiovFQk3y/YVd9sgqx7teEL6HMvf9AyUUC1iOrWPM=",
-	//
-	// 				// The Pagefind highlight script
-	// 				"sha256-6JuOYKInzyFw8q+GPQrZTQbAfw4q6G8S9jxNRH02ueg=",
-	// 			],
-	// 		},
-	// 		styleDirective: {
-	// 			resources: [
-	// 				"'self'",
-	//
-	// 				// Needed for the hash below to work
-	// 				"'unsafe-hashes'",
-	// 			],
-	// 			hashes: [
-	// 				//
-	//
-	// 				// Not sure what this style is for, but it's required
-	// 				"sha256-y0FDWhr0m3Jam1HNNCTVey61m4zSmlv57Q1pjAE7A+E=",
-	// 			],
-	// 		},
-	// 	},
-	// },
+	experimental: {
+		csp: {
+			algorithm: "SHA-512",
+			directives: [
+				"default-src 'none'",
+				"img-src 'self'",
+				"font-src 'self'",
+				"connect-src 'self'",
+				"manifest-src 'self'",
+				"object-src 'none'",
+				"frame-ancestors 'none'",
+				"form-action 'none'",
+				"base-uri 'none'",
+			],
+			scriptDirective: {
+				resources: [
+					"'self'",
+
+					// For pagefind to work
+					"'wasm-unsafe-eval'",
+				],
+
+				hashes: [
+					//
+
+					// The script to use pagefind to search the site
+					"sha256-LzkiovFQk3y/YVd9sgqx7teEL6HMvf9AyUUC1iOrWPM=",
+
+					// The pagefind highlight script
+					"sha256-6JuOYKInzyFw8q+GPQrZTQbAfw4q6G8S9jxNRH02ueg=",
+				],
+			},
+			styleDirective: {
+				resources: [
+					"'self'",
+
+					// Needed for the hash below to work
+					"'unsafe-hashes'",
+				],
+				hashes: [
+					//
+
+					// Not sure what this style is for, but it's required
+					"sha256-y0FDWhr0m3Jam1HNNCTVey61m4zSmlv57Q1pjAE7A+E=",
+				],
+			},
+		},
+	},
 
 	vite: {
 		plugins: [
