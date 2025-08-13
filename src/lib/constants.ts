3c78ac94f5cffe4c7236d5f60151feb5ef410bd8
Hankertrix <91734413+hankertrix@users.noreply.github.com>
2025-08-13 10:11:29 +0800

    Add constants for pagefind

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -8,3 +8,9 @@ export const PAGES = {
 	Categories: null,
 	Signs: null,
 };
+
+// The pagefind base path
+export const PAGEFIND_BASE_PATH = "/pagefind";
+
+// The pagefind highlight parameter
+export const PAGEFIND_HIGHLIGHT_PARAM = "highlight";
