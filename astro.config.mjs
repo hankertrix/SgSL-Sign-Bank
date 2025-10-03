08d0719b5e07694e11afb25fc24431b7c3348706
Hankertrix <91734413+hankertrix@users.noreply.github.com>
2025-10-04 03:09:31 +0800

    Removed rm -rf and use the eager dev strategy for vite plugin pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -78,7 +78,7 @@ export default defineConfig({
                 assetsDirectory: "public",
                 bundleDirectory: "pagefind",
                 buildScript: "build",
-                developStrategy: "lazy",
+                developStrategy: "eager",
             }),
         ],
     },
